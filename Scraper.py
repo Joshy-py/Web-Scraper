679053d904149bc6ae4fa4ff528fe2a351b6879e
Iremide Akoda <ijakoda2@gmail.com>
2024-11-02 17:17:43 -0400

    Fetching the url

--- Scraper.py
+++ Scraper.py
@@ -3,3 +3,11 @@ from bs4 import BeautifulSoup
 import pandas as pd
 import schedule
 import time
+
+url = "https://www.cnn.com/business/tech"
+response = requests.get(url)
+
+if (response.status_code == 200):
+    content = response.text
+else:
+    print("Failed to retreive page, maybe it's down?")
