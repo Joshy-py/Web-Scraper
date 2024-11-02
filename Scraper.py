c32db5329ba47c2431ffac54894d90d2277310ae
Iremide Akoda <ijakoda2@gmail.com>
2024-11-02 17:31:04 -0400

    scraping headlines !!

--- Scraper.py
+++ Scraper.py
@@ -11,3 +11,9 @@ if (response.status_code == 200):
     content = response.text
 else:
     print("Failed to retreive page, maybe it's down?")
+
+soup = BeautifulSoup(content, "html_parser")
+data = []
+
+for hl in soup.find_all("span", class_="container__headline-text"):
+    data.append(hl.get_text())
