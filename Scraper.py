5b22ee6222552d05b9abf580d2e2b33696b1783d
Iremide Akoda <ijakoda2@gmail.com>
2024-11-03 09:57:21 -0500

    Output more readable

--- Scraper.py
+++ Scraper.py
@@ -7,7 +7,6 @@ import time
 def data_scraping():
     url = "https://www.cnn.com/business/tech"
     response = requests.get(url)
-
     if (response.status_code == 200):
         content = response.text
     else:
@@ -15,15 +14,17 @@ def data_scraping():
 
     soup = BeautifulSoup(content, "html.parser")
     hl = soup.find_all("span", class_="container__headline-text")
-    #for i in range(len(hl)):
+    # for i in range(len(hl)):
     #    print(hl[i].text)
     print("Scraping headlines...")
     sd = pd.DataFrame(hl, columns=["Headlines"])
-    sd.to_csv("CNN_Headlines.csv", index=False)
+    sd.index = sd.index.map(lambda x:"|index " + str(x))
+    sd.iloc[:,0] = sd.iloc[:,0].apply(lambda x:"| " + str(x))
+    sd.to_csv("CNN_Headlines.csv", sep=" ", header=None, index=False)
 
 data_scraping()
 
-schedule.every().day.at("08:00").do(data_scraping)
+schedule.every().day.at("12:00").do(data_scraping)
 
 while True:
     schedule.run_pending()
